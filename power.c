752862e9c69d1f06518ccacba8e18696a6f95abf
sourya <souryadeepsen80@gmail.com>
2021-09-22 00:50:27 -0400

    upload pow program solution to git

---
+++ power.c
@@ -0,0 +1,28 @@
+/*
+leetcode math problem 50. Pow(x,n)
+this code hits a segmentation fault for very large n, as stack size increases by a lot!
+please submit a pull request if you have a solution to this. I saw there is a fast power recursive algo, still to explore that
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+double myPow(double,int);
+
+void main(){
+
+	double res = myPow(2,2147483647);	//chose value of n wisely, to prevent segmentation fault
+	printf("%d\n",INT_MAX);
+	printf("Result is: %f\n",res);
+
+}
+
+double myPow(double x, int n){
+
+	if (n)
+        return n>0?x*myPow(x,n-1):(1/x)*myPow(x,n+1);
+    else
+        return 1;
+
+}
