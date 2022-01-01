bbfce0e42e503fb6172338bc09c242d2b6dc8266
sourya <souryadeepsen80@gmail.com>
2022-01-01 13:30:10 -0800

    check in leet solution for reverse of linked list

--- src/reverselist.c
+++ src/reverselist.c
@@ -1,13 +1,42 @@
+//program: revereselist.c
+//leetcode problem: https://leetcode.com/problems/reverse-linked-list/
+/*
+	use header list1.h from ../include
+	compile with list1.c which has list1.h function implementations
+	Runtime: 0 ms, faster than 100.00% of C online submissions for Reverse Linked List.
+	Memory Usage: 6.2 MB, less than 90.23% of C online submissions for Reverse Linked List.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "list1.h"
 
 struct ListNode* reverseList(struct ListNode* head){
+
+	struct ListNode *prev = NULL;
+	struct ListNode *current = head;
+	struct ListNode *next = NULL;
+    
+	while(current){
+        	next = current->next;
+        	current->next = prev;
+        	prev = current;
+        	current = next;
+    	}
+    
+	return prev;
 
 }
 
 int main(int argc, char* argv[]){
 
+	struct ListNode *head = NULL;
+        int i;
+        for (i = 1; i < argc; i++) {
+                head = prepend(head, *argv[i]);
+        }
 
+        print(head);
+        deallocate_list(head);
 
 }
