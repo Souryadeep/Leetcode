94da189a0ae4ebc9ed74f17dd53ab3e7ae32587d
sourya <souryadeepsen80@gmail.com>
2022-01-02 12:26:36 -0800

    check in template for hamming weight leetcode problem

---
+++ src/hammingWeight.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+int hammingWeight(uint32_t n) {
+    
+}
+
+int main(int argc, char* argv[]){
+
+	if (argc<2){
+		printf("usage: ./test <number>");
+		exit(1);
+	}
+
+	return 0;
+
+}
