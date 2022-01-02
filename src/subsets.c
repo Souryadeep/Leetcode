7f56f92b53dbbcb5b6f38f8d6ec84c00547af601
sourya <souryadeepsen80@gmail.com>
2022-01-02 15:52:51 -0800

    check in skeleton of subsets leetcode problem

---
+++ src/subsets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+	
+int** subsets(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+
+
+
+}
+
+
+int main(int argc, char * argv[]){
+
+
+	return 0;
+}
