d346d220d44d5ca585ec873a9a58c8227fb1b4e8
sourya <souryadeepsen80@gmail.com>
2022-01-02 22:49:20 -0800

    check in skeleton for print in order concurrency leet problem

---
+++ src/printinorder.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <pthreads.h>
+
+typedef struct {
+    // User defined data may be declared here.
+    
+} Foo;
+
+Foo* fooCreate() {
+    Foo* obj = (Foo*) malloc(sizeof(Foo));
+    
+    // Initialize user defined data here.
+    
+    return obj;
+}
+
+void first(Foo* obj) {
+    
+    // printFirst() outputs "first". Do not change or remove this line.
+    printFirst();
+}
+
+void second(Foo* obj) {
+    
+    // printSecond() outputs "second". Do not change or remove this line.
+    printSecond();
+}
+
+void third(Foo* obj) {
+    
+    // printThird() outputs "third". Do not change or remove this line.
+    printThird();
+}
+
+void fooFree(Foo* obj) {
+    // User defined data may be cleaned up here.
+    
+}
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+	return 0;
+
+}
