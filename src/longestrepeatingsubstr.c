334f2923fa2c1ba1f25584eefa04077fe41ab070
sourya <souryadeepsen80@gmail.com>
2022-01-15 14:15:34 -0500

    add template for longest repeating substring leetcode problem

---
+++ src/longestrepeatingsubstr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//other headers or macros go here
+
+
+int longestRepeatingSubstring(char * s){
+
+}
+
+
+int main(int argc, char *argv[]){
+    return 0;
+}
+
