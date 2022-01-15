7c283aaa80bcb5152b60b75c4c0a7a192d9e5a22
sourya <souryadeepsen80@gmail.com>
2022-01-14 22:06:08 -0500

    add partial working solution for longest substring without repetition. fails for multiple test cases

--- src/lengthoflongestsubstr.c
+++ src/lengthoflongestsubstr.c
@@ -4,12 +4,50 @@
 //other headers or macros go here
 
 
-int lengthofLongestSubstring(char* s){
+void initarr(char *t,int len){
+    
+    for(int i=0;i<len;++i){
+        t[i] = 0;
+    }
+}
 
+int lengthOfLongestSubstring(char * s){
+
+    char t[strlen(s)+1];
+    int len = strlen(s)+1;
+    
+    initarr(t,len);
+    int i = 0;
+    int maxlen = 0;
+    while(*s){
+        
+        if(strchr(t,*s)){
+            if(i>=maxlen){
+                maxlen = i;
+                i=0;
+                initarr(t,len);
+            }
+        }
+        t[i] = *s;
+        i++;
+        s++;
+    }
+    if(i>=maxlen)
+        maxlen = i;
+    return maxlen;
 }
 
 int main(int argc, char *argv[]){
+		
+	if(argc<2){
+		printf("usage: ./test <string>\n");
+		exit(1);
+	}
 
-    return 0;
+	char *p = argv[1];
+	int len = lengthOfLongestSubstring(p);
+
+	printf("length of longest substring without repitition is: %d\n",len);
+	return 0;
 }
 
