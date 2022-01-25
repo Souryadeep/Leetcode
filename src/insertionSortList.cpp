15459fbfca7b07513ad89bdb8f0fbc714816d794
sourya <souryadeepsen80@gmail.com>
2022-01-25 11:25:46 -0500

    add insertionsort list cpp leetcode solution to git.

--- src/insertionSortList.cpp
+++ src/insertionSortList.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
+/*
+	program: insertionSortList.cpp
+	leetcode: https://leetcode.com/problems/insertion-sort-list/
+	Runtime: 83 ms, faster than 14.72% of C++ online submissions for Insertion Sort List
+	Memory Usage: 10.2 MB, less than 6.43% of C++ online submissions for Insertion Sort List
+*/
 
+#include <iostream>
+using namespace std;
 
  struct ListNode {
       int val;
       ListNode *next;
+
       ListNode() : val(0), next(nullptr) {}
       ListNode(int x) : val(x), next(nullptr) {}
       ListNode(int x, ListNode *next) : val(x), next(next) {}
@@ -37,10 +45,37 @@ public:
     }
 };
 
+struct ListNode* add(ListNode* head,int val){
 
-int main(int argc, char*argv){
+	struct ListNode *node = new ListNode(val,head);
+	if (node == NULL)
+		  return NULL;
+	return node;
+}
 
+int main(int argc, char* argv[]){
 
+	cout << "Enter size of list: ";
+	int size;
+
+	cin >> size;
+	
+	cout << "Enter the elements of the list: \n";
+	ListNode *head = new ListNode();
+
+	for(auto i = 0;i<size;++i){
+		int temp;
+		cin >> temp;
+		head = add(head,temp);	
+	}
+	Solution obj;
+	head = obj.insertionSortList(head);
+	head = head->next;
+	for(auto i = 0;i<size;++i){
+		cout << head->val <<  " ";
+		head = head->next;
+	} 	
+	cout << endl;
 
 	return 0;
 }
